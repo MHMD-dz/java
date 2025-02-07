c80338883a63891b8dc710fda47c84d0a6e23a3d
Mohamed <mhmdhayza@gmail.com>
2025-02-07 23:03:54 +0100

    adding variables and printing theme

--- YouTube_crs/Main.java
+++ YouTube_crs/Main.java
@@ -5,5 +5,29 @@ public class Main {
         System.out.println("I hate my");
         System.out.println("I hate jave it's a trashy language");
         System.out.println("What am I doing whit my life ?? ");
+        System.out.println("\n\n");
+        // using variable 
+        int age , year;
+        age = 21 ;
+        year = 2003 ;
+        double temp , price ;
+        temp = 44.5 ;
+        char monye , monye2 ;
+        monye = 'E' ;
+        monye2 = '$' ;
+        String name , car , color ;
+        name = "Mohamed" ;
+        car = "AUDI RS7" ;
+        color = "black" ;
+        price = 128600.99 ;
+        System.out.print("my age is : ");
+        System.out.println(age);
+        System.out.println("And I was borne in : " + year);
+        System.out.println("my brain temperuter after starting to learn java is : " + temp);
+        System.out.println("my salary will be in " + monye + " and " + monye2 + " in shaa allah");
+        System.out.println("and my name is : " + name );
+        System.out.println("My dream car is : " + color + " " + car );
+        System.out.println("it will coust my " + price + " " + monye2);
+        System.out.println("yah don't even dream of geting it dream about closing S4 ");
     }
 }
