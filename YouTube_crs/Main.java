126dcd3a6bc89da4e132e37fb10e95f256636823
Mohamed <mhmdhayza@gmail.com>
2025-02-07 22:05:17 +0100

    what is this trash

---
+++ YouTube_crs/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    // my first java programm
+    public static void main (String[] args) { 
+        System.out.println("I hate my");
+        System.out.println("I hate jave it's a trashy language");
+        System.out.println("What am I doing whit my life ?? ");
+    }
+}
--- YouTube_crs/hi.java
+++
@@ -1,1 +0,0 @@
-test ripo push
