b104451c63df5a83a6428e410945b8a6d3f13864
Mohamed <mhmdhayza@gmail.com>
2025-02-13 20:59:00 +0100

    add more inputs

--- YouTube_crs/inputs.java
+++ YouTube_crs/inputs.java
@@ -7,16 +7,30 @@ public class inputs {
         // declaring the variables
         String first_name , last_name , car_name ;
         int age , salary;
-        System.out.println("Enter your first name : ");
+        float gpa ;
+        boolean loser ;
+        System.out.print("Enter your first name : ");
         first_name = scanner.nextLine();
-        System.out.println("Enter your last name : ");
+        System.out.print("Enter your last name : ");
         last_name = scanner.nextLine();
-        System.out.println("Enter your age : ");
-        //age = scanner.nextLine();
-        System.out.println("Enter your dream car name : ");
+        System.out.print("Enter your age : ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter your dream car name : ");
         car_name = scanner.nextLine();
+        System.out.print("Enter your salary : ");
+        salary = scanner.nextInt();
+        System.out.print("what is your gpa : ");
+        gpa = scanner.nextFloat();
+        System.out.println("are you a loser : (true/false)");
+        loser = scanner.nextBoolean();
+        System.out.println();
 
         System.out.println("Hallo " + last_name + " " + first_name);
+        System.out.println("I hope you get your " + car_name + " in the futur");
+        System.out.println("and it's not bad salary as " + salary + " DA for semoan in his " + age);
+        System.out.println("and your gpa is : " + gpa);
+        System.out.println("you are a loser : " + loser);
 
 
 
