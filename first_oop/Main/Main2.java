782da94841355a4dc5094691474ce8ea7c3aedad
Mohamed <mhmdhayza@gmail.com>
2025-02-26 23:11:32 +0100

    creatin new main for the Circla class and using it as object

---
+++ first_oop/Main/Main2.java
@@ -0,0 +1,37 @@
+package first_oop.Main;
+import first_oop.packeges.Circle;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main (String[] arge){
+        // creating the object
+        Circle test = new Circle() ;
+        Scanner scan = new Scanner(System.in);
+
+        // variables 
+        float radius ;
+        String color ;  
+
+        // user input
+        System.out.print("Enter the circle radius: ");
+        radius = scan.nextFloat(); 
+        scan.nextLine();
+        System.out.print("Enter the circle color: ");
+        color = scan.nextLine(); 
+        
+        // seting
+        test.set_Radius(radius);
+        test.set_Color(color);
+
+        // output 
+        System.out.println("The cicle radius is: " + test.get_Radius());
+        System.out.println("The cicle color is: " + test.get_Color());
+        System.out.printf("The cicle aria is: %.2f\n" , test.get_Area());
+        System.out.printf("The cicle circumference is: %.2f\n" , test.get_Circumference());
+
+
+
+        scan.close();
+    }
+    
+}
