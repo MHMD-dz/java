23153e8a1035eeb14f775a56b169856ccdc2d0b7
Mohamed <mhmdhayza@gmail.com>
2025-02-26 23:10:03 +0100

    adding a new class for circle

---
+++ first_oop/packeges/Circle.java
@@ -0,0 +1,37 @@
+package first_oop.packeges;
+
+public class Circle {
+    // variables 
+    private float radius ;
+    private String color ;
+
+    // function
+
+    // set
+    public void set_Radius(float rad){
+        radius = rad ;
+    }
+
+    public void set_Color(String col){
+        color = col ;
+    }
+
+    // get 
+    public float get_Radius(){
+        return radius ;
+    }
+
+    public String get_Color(){
+        return color ;
+    }
+
+    public double get_Area(){
+        return ( Math.PI * (Math.pow(radius, 2)) ) ;
+    }
+
+    public double get_Circumference(){
+        return ( 2 * Math.PI * radius ) ;
+    }
+
+    
+}
