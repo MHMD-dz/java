77360c35af229e28ffd79a421812563be8f42348
Mohamed <mhmdhayza@gmail.com>
2025-03-16 23:01:24 +0200

    i started somting about static and i cant finich it i hate my

---
+++ therd_oop/Main.java
@@ -0,0 +1,13 @@
+package therd_oop;
+
+public class Main {
+    public static void main(String[] arg){
+        // declaring the object
+        Student etd1 = new Student() ;
+
+        // output
+        System.out.println();
+
+
+    }
+}
---
+++ therd_oop/Student.java
@@ -0,0 +1,27 @@
+package therd_oop;
+
+public class Student {
+    // variable 
+    private String name ;
+    private String univ ;
+    private static String fild = "info" ;
+    private int number ;
+    private int cont = 1 ;
+    private String pass ;
+
+    // constracter
+    public Student(){
+        this.name = "mohamed" ;
+        this.univ = "la fack" ;
+    }
+
+    // function 
+    public boolean isLogIn( int numb , String pas ){
+        if (condition) {
+            
+        }
+
+    }
+
+    
+}
