9ccac9b4caa4befa6cd3f8ad44366bc28401ff60
Mohamed <mhmdhayza@gmail.com>
2025-03-19 21:59:29 +0100

    static methode

--- therd_oop/Main.java
+++ therd_oop/Main.java
@@ -1,13 +1,32 @@
 package therd_oop;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] arg){
         // declaring the object
-        Student etd1 = new Student() ;
+        Student etd1 = new Student( "mohamed" , "1234") ;
+        Student etd2 = new Student( "fekir" , "6548") ;
+        Student etd3 = new Student( "amine" , "9876") ;
+        Student etd4 = new Student( "laila" , "2525") ;
+        Scanner scan = new Scanner(System.in) ;
+
+        // variable
+        int number ;
+        String  password ;
+
+        // input
+        System.out.print("Enter the student number: ");
+        number = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter the student password: ");
+        password = scan.nextLine();
 
         // output
-        System.out.println();
+        System.out.println(etd2.getNumber());
+        System.out.println("is the student loged in: " + etd1.isLogIn(number, password));
+        
 
 
+
+        scan.close();
     }
 }
--- therd_oop/Student.java
+++ therd_oop/Student.java
@@ -1,4 +1,5 @@
 package therd_oop;
+import java.util.Objects;
 
 public class Student {
     // variable 
@@ -6,22 +7,84 @@ public class Student {
     private String univ ;
     private static String fild = "info" ;
     private int number ;
-    private int cont = 1 ;
+    private static int cont = 1 ;
     private String pass ;
 
     // constracter
-    public Student(){
-        this.name = "mohamed" ;
-        this.univ = "la fack" ;
+    public Student( String namec , String passc){
+        this.number = cont ;
+        this.name = namec ;
+        this.pass = passc ;
+        cont++;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUniv() {
+        return this.univ;
+    }
+
+    public void setUniv(String univ) {
+        this.univ = univ;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPass() {
+        return this.pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public Student name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public Student univ(String univ) {
+        setUniv(univ);
+        return this;
+    }
+
+    public Student number(int number) {
+        setNumber(number);
+        return this;
+    }
+
+    public Student pass(String pass) {
+        setPass(pass);
+        return this;
+    }
+
 
     // function 
     public boolean isLogIn( int numb , String pas ){
-        if (condition) {
-            
+        if (this.number == numb && this.pass.equals(pas)) {
+            return true ;
         }
-
+        return false ;
     }
 
-    
+    public String toString() {
+        return "{" +
+            " name='" + getName() + "'" +
+            ", univ='" + getUniv() + "'" +
+            ", number='" + getNumber() + "'" +
+            ", pass='" + getPass() + "'" +
+            "}";
+    }
 }
